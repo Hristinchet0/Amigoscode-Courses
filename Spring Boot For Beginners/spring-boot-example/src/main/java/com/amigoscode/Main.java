8a94999538981a70b2acff8adf31c3eca8025d48
Hristinchet0 <bansko.sport@gmail.com>
2023-04-27 10:19:26 +0300

    Add

---
+++ Spring Boot For Beginners/spring-boot-example/src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,67 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/customers")
+
+public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody Customer customerDetails) {
+        Customer updatedCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Customer not exist with id: " + id));
+
+        updatedCustomer.setName(customerDetails.getName());
+        updatedCustomer.setEmail(customerDetails.getEmail());
+        updatedCustomer.setAge(customerDetails.getAge());
+
+        customerRepository.save(updatedCustomer);
+
+    }
+
+
+}
